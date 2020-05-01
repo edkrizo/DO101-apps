359dd45bb11b3c17fe9cc0d8aeca06498b39f706
edkrizo <edoug84@gmail.com>
2020-05-01 15:04:52 +0000

    updated appication to version 3 stable

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app. and I am running from Openshift!' + '\n';
+    response = 'This is version 3 of the app. Openshift is really easy to work with' + '\n';
 
     //send the response to the client
     res.send(response);
